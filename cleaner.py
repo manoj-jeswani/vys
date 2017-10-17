913ac8f062254673668abc4598f8bf78b2c24a7a
Manoj Jeswani <manoj.jeswani@mettl.com>
2017-10-17 17:07:55 +0530

    yippppe

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,25 @@
 import os
-os.system("cd ~/Documents/gomp3/vys/static/logical")
-os.system("rm -r d_audios")
-os.system("mkdir d_audios")
-os.system("rm -r d_videos")
-os.system("mkdir d_videos")
+# from datetime import datetime
+# from threading import Timer
+
+
+
+
+# x=datetime.today()
+# y=x.replace(day=x.day+1, hour=3, minute=0, second=0, microsecond=0)
+# delta_t=y-x
+
+# secs=delta_t.seconds+1
+
+
+def clean_fun():
+
+	os.system("cd ~/vys/static/logical && rm -r d_audios")
+	os.system("cd ~/vys/static/logical && mkdir d_audios")
+	os.system("cd ~/vys/static/logical && rm -r d_videos")
+	os.system("cd ~/vys/static/logical && mkdir d_videos")
+
+# t = Timer(secs, clean_fun)
+# t.start()
+
+clean_fun()
